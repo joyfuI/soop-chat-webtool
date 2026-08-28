9681d8b8019da54deff59445cd75424ac13e239d
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-08-29 04:38:39 +0900

    FormLabel 컴포넌트 디자인 수정

--- packages/ui/src/FormLabel.tsx
+++ packages/ui/src/FormLabel.tsx
@@ -14,7 +14,11 @@ const FormLabel = ({ children, label, description }: FormLabelProps) => {
         {label}
       </Typography>
       {description ? (
-        <Typography gutterBottom sx={{ display: 'block' }} variant="caption">
+        <Typography
+          gutterBottom
+          sx={{ display: 'block', whiteSpace: 'pre-wrap' }}
+          variant="body2"
+        >
           {description}
         </Typography>
       ) : null}
